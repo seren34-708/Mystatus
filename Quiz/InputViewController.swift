b2a4ab9094c54f96405a211eb3c2c7a0fc1c4435
Naoya <nanetto777@gmail.com>
2022-01-29 16:30:11 +0900

    アクティビティリストの完成

--- Quiz/InputViewController.swift
+++ Quiz/InputViewController.swift
@@ -4,32 +4,60 @@ class InputViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     @IBOutlet weak var tableView: UITableView!
     
-    let todoList = ["牛乳を買う", "掃除をする", "アプリ開発の勉強をする"]
-
+    var todoList = [String]()
+    let userDefaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let storedTodoList = userDefaults.array(forKey: "todoList") as? [String] {
+                todoList.append(contentsOf: storedTodoList)
+        }
     }
     
+    //リストの追加
     @IBAction func addBtnAction(_ sender: Any){
-        
+        let alertController = UIAlertController(title: "ToDo追加", message: "ToDoを入力してください。", preferredStyle: UIAlertController.Style.alert)
+        alertController.addTextField(configurationHandler: nil)
+        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (acrion: UIAlertAction) in
+            //OKをタップした時の処理
+            if let textField = alertController.textFields?.first {
+                self.todoList.insert(textField.text!, at: 0)
+                self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: UITableView.RowAnimation.right)
+                //ToDoを保存
+                self.userDefaults.set(self.todoList, forKey: "todoList")
+            }
+        }
+        alertController.addAction(okAction)
+        let cancelButton = UIAlertAction(title: "CANCEL", style: UIAlertAction.Style.cancel, handler: nil)
+        alertController.addAction(cancelButton)
+        present(alertController, animated: true, completion: nil)
     }
     
-    @IBAction func backButton(_ sender: Any) {
-        self.presentingViewController?.dismiss(animated: true)
-    }
-    
-    //UITableViewDataSource
+    //UITableViewDataSource　リストのカウント
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todoList.count
     }
     
-    //UITableViewDelegate
+    //UITableViewDelegate　リストの表示
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // セルを取得する
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         // セルに表示する値を設定する
         cell.textLabel!.text = todoList[indexPath.row]
         return cell
+    }
+    
+    //リストの削除機能
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            todoList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
+            userDefaults.set(todoList, forKey: "todoList")
+        }
+    }
+    
+    
+    @IBAction func backButton(_ sender: Any) {
+        self.presentingViewController?.dismiss(animated: true)
     }
 }
